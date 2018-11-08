97d18a0e39d03f36b6f0ecda6462694082fbcb2d
Andrius Mudinas <andrius@wikiled.com>
2018-11-08 17:07:20 +0000

    Adding null object

---
+++ src/Wikiled.Sentiment.Api/Service/NullSentimentAnalysis.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Wikiled.Sentiment.Api.Request;
+using Wikiled.Text.Analysis.Structure;
+
+namespace Wikiled.Sentiment.Api.Service
+{
+    public class NullSentimentAnalysis : ISentimentAnalysis
+    {
+        public Task<Document> Measure(SingleRequestData document, CancellationToken token)
+        {
+            return Task.FromResult((Document)null);
+        }
+
+        public IObservable<Document> Measure(SingleRequestData[] documents, CancellationToken token)
+        {
+            return Observable.Empty<Document>();
+        }
+
+        public Task<Document> Measure(string text, CancellationToken token)
+        {
+            return Task.FromResult((Document)null);
+        }
+
+        public Task<double?> Measure(string text)
+        {
+            return Task.FromResult((double?)null);
+        }
+    }
+}
