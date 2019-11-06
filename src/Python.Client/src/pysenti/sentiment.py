8dce6014e252b4065e68923b2b413aa20a531aac
Andrius Mudinas <keistokas@gmail.com>
2019-11-06 18:48:57 +0000

    update

--- src/Python.Client/src/pysenti/sentiment.py
+++ src/Python.Client/src/pysenti/sentiment.py
@@ -13,6 +13,7 @@ class SentimentConnection(object):
     def __init__(self, clientId):
         if clientId is None or len(clientId) < 10:
             raise ValueError('Client id is too short. Minimum 10 symbols')
+
         self.clientId = clientId
         self.host = 'sentiment.wikiled.com'
         # self.host = 'localhost:63804'
@@ -21,44 +22,22 @@ class SentimentConnection(object):
         broker_port = 1883
 
         self.message = {}
+        logger.info(f'Setting up connection to {self.host} for {clientId}')
+        self.sentiment_result_topic = 'Sentiment/Result/' + clientId
+        self.message_topic = 'Message/' + clientId
+        self.error_topic = 'Error/' + clientId
 
         self.client = mqtt.Client(client_id=clientId)
         self.connection = self.client.connect(broker_url, broker_port)
         self.client.on_message = self._on_message
-        self.client.on_disconnect = self_on_disconnect
-        self.client.subscribe('Sentiment/Result/' + clientId, qos=0)
-        self.client.subscribe('Error/' + clientId, qos=0)
-        self.client.subscribe('Message' + clientId, qos=0)
+        self.client.on_disconnect = self._on_disconnect
+        self.client.subscribe(self.sentiment_result_topic, qos=0)
+        self.client.subscribe(self.error_topic, qos=0)
+        self.client.subscribe(self.message_topic, qos=0)
 
         self.client.loop_start()
         self._load()
 
-
-    # def process_sentiment(self, documents, domain, lexicon, clean):
-    #
-    #
-    #     index = 0
-    #     processed_ids = {}
-    #     batch_request_documents = []
-    #     for document in self.documents:
-    #         id = str(uuid.uuid4())
-    #         batch_request_documents.append({'text': document, 'id': id})
-    #         processed_ids[id] = index
-    #         index += 1
-    #         if len(batch_request_documents) >= 200:
-    #             self.client.publish('Sentiment/Analysis', json.dumps(save_batch, indent=2))
-    #             batch_request_documents = []
-    #             print('Processed {}'.format(index))
-    #
-    #
-    #     for document in self.documents:
-    #         id = str(uuid.uuid4())
-    #         batch_request_documents.append({'text': document, 'id': id})
-    #         processed_ids[id] = index
-    #
-    #     # save_batch = {}
-    #     # save_batch['documents'] = batch_request_documents
-    #     # save_batch['Name'] = 'Test'
 
     def _load(self):
         with Session() as session:
@@ -68,39 +47,18 @@ class SentimentConnection(object):
             self.supported_domains = json.loads(session.get(url).content)
 
     def _on_disconnect(self, client, userdata, rc=0):
-        logging.debug("Disconnected result code " + str(rc))
+        logger.info("Disconnected result code " + str(rc))word
         client.loop_stop()
 
     def _on_message(self, client, userdata, message):
         if message.topic not in self.message:
             self.message[message.topic] = []
-        print("message received ", str(message.payload.decode("utf-8")))
-        print("message topic=", message.topic)
-        print("message qos=", message.qos)
-        print("message retain flag=", message.retain)
+        logger.info("message received ", str(message.payload.decode("utf-8")))
+        logger.debug("message topic=", message.topic)
+        logger.debug("message qos=", message.qos)
+        logger.debug("message retain flag=", message.retain)
         self.message[message.topic].append(message)
 
-
-
-        # with Session() as session:
-        #     url = 'http://{}/api/sentiment/parsestream'.format(self.host)
-        #     data['documents'] = batch_request_documents
-        #     json_object = json.dumps(data, indent=2)
-        #     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        #     with session.post(url, json_object, headers=headers, stream=True) as r:
-        #         for line in r.iter_lines():
-        #             # filter out keep-alive new lines
-        #             if line:
-        #                 decoded_line = line.decode('utf-8')
-        #                 sentimen_result = json.loads(decoded_line)
-        #                 sentiment_class = 0
-        #                 if sentimen_result['Stars'] is not None:
-        #                     if sentimen_result['Stars'] > 3:
-        #                         sentiment_class = 1
-        #                     else:
-        #                         sentiment_class = -1
-        #                 id = processed_ids[sentimen_result['Id']]
-        #                 yield (id, sentiment_class, sentimen_result)
 
 
 class SentimentAnalysis(object):
@@ -134,18 +92,19 @@ class SentimentAnalysis(object):
             yield from self.process_on_server(batch_request_documents, processed_ids)
 
     def _process_on_server(self, batch_request_documents, processed_ids):
-        document_request = _create_batch(batch_request_documents)
-        self.client.publish('Sentiment/Analysis', json.dumps(document_reques, indent=2))
+        document_request = self._create_batch(batch_request_documents)
+        self.client.publish('Sentiment/Analysis', document_request)
         while len(processed_ids) > 0:
-        pass
+            while self.connection.:
+            time.sleep(1)
 
     def _create_batch(self, documents):
         data = {}
-        data['CleanText'] = clean
+        data['CleanText'] = self.clean
         if self.lexicon is not None:
-            data['dictionary'] = lexicon
+            data['dictionary'] = self.lexicon
         if self.domain is not None:
-            data['domain'] = domain
+            data['domain'] =self. domain
         data['documents'] = documents
         return json.dumps(data, indent=2)
 
@@ -166,3 +125,49 @@ if __name__ == "__main__":
     sentiment = SentimentAnalysis(documents, dictionary, domain='market', clean=True)
     for result in sentiment:
         print(result)
+
+    # def process_sentiment(self, documents, domain, lexicon, clean):
+    #
+    #
+    #     index = 0
+    #     processed_ids = {}
+    #     batch_request_documents = []
+    #     for document in self.documents:
+    #         id = str(uuid.uuid4())
+    #         batch_request_documents.append({'text': document, 'id': id})
+    #         processed_ids[id] = index
+    #         index += 1
+    #         if len(batch_request_documents) >= 200:
+    #             self.client.publish('Sentiment/Analysis', json.dumps(save_batch, indent=2))
+    #             batch_request_documents = []
+    #             print('Processed {}'.format(index))
+    #
+    #
+    #     for document in self.documents:
+    #         id = str(uuid.uuid4())
+    #         batch_request_documents.append({'text': document, 'id': id})
+    #         processed_ids[id] = index
+    #
+    #     # save_batch = {}
+    #     # save_batch['documents'] = batch_request_documents
+    #     # save_batch['Name'] = 'Test'
+
+    # with Session() as session:
+    #     url = 'http://{}/api/sentiment/parsestream'.format(self.host)
+    #     data['documents'] = batch_request_documents
+    #     json_object = json.dumps(data, indent=2)
+    #     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    #     with session.post(url, json_object, headers=headers, stream=True) as r:
+    #         for line in r.iter_lines():
+    #             # filter out keep-alive new lines
+    #             if line:
+    #                 decoded_line = line.decode('utf-8')
+    #                 sentimen_result = json.loads(decoded_line)
+    #                 sentiment_class = 0
+    #                 if sentimen_result['Stars'] is not None:
+    #                     if sentimen_result['Stars'] > 3:
+    #                         sentiment_class = 1
+    #                     else:
+    #                         sentiment_class = -1
+    #                 id = processed_ids[sentimen_result['Id']]
+    #                 yield (id, sentiment_class, sentimen_result)
