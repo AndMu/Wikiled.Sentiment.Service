8ce338183cb3730512c48935b7bf996e0a6ddb8c
Andrius Mudinas <keistokas@gmail.com>
2020-07-16 11:59:53 +0100

    Bump version

--- src/Python.Client/src/psenti/__init__.py
+++ src/Python.Client/src/psenti/__init__.py
@@ -4,7 +4,7 @@ from .helpers.logging_helpers import LoggingFileHandler
 
 __name__ = "psenti"
 
-__version__ = '0.0.0'
+__version__ = '1.0.0'
 version = __version__
 
 __all__ = ['SentimentConnection', 'SentimentAnalysis', 'LoggingFileHandler', 'Document', 'SentimentAnalysis']
